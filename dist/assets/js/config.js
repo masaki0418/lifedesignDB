5b03d867f32f1bd7af23de241fde5dbc6693f8f4
田中勝明 <receive@azurea.biz>
2023-04-12 16:52:14 +0900

    フォーム製作中。明日完成させること！

--- dist/assets/js/config.js
+++ dist/assets/js/config.js
@@ -1,1 +1,1 @@
-jQuery((function(i){i(".list-office .parent").on("click",(function(){i(this).next().slideToggle(),i(this).toggleClass("active")}))}));
+jQuery((function(t){t(".list-office .parent").on("click",(function(){t(this).next().slideToggle(),t(this).toggleClass("active")})),t("input").on("change",(function(){var i=t(this).prop("files")[0];t("p").text(i.name)}))}));
--- source/js/config.js
+++ source/js/config.js
@@ -3,4 +3,8 @@ jQuery(function($) {
 		$(this).next().slideToggle();
 		$(this).toggleClass("active");//追加部分
 	});
+	$('input').on('change', function () {
+	    var file = $(this).prop('files')[0];
+	    $('p').text(file.name);
+	});
 });
